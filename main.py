9ecf7cdf3b7e205f657e1e6b2b03c09f3768114e
th3unkn0n <40244622+th3unkn0n@users.noreply.github.com>
2020-05-28 18:48:47 +0530

    tags
    list most used tags

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-u", "--username", required=True, help="username of account to scan")
 ap.add_argument("-p", "--postscrap", action='store_true', help="scrape all uploaded images info ")
 ap.add_argument("-s", "--savedata", action='store_true', help="save data to file")
+ap.add_argument("-t", "--tagscrap", action="store_true", help="list often used tags")
 args = vars(ap.parse_args())
 	
 os.system("clear")
 ig = main(user=args["username"])
-ig.print_data()
+if args['tagscrap']:
+	ig.print_data()
+else:
+	ig.print_data_()
 
 if args['postscrap']:
 	ig.scrap_uploads()

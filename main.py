ebc9303648a3486f031ccdfe4759173b2e6b8f9b
th3unkn0n <40244622+th3unkn0n@users.noreply.github.com>
2020-10-30 20:43:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ from api import *
 ap = argparse.ArgumentParser()
 ap.add_argument("-u", "--user", required=True, help="username of account to scan")
 ap.add_argument("-p", "--post", action="store_true", help="image info of user uploads")
-# ap.add_argument("-t", "--tor", action="store_true", help="make all requests over tor")
-# ap.add_argument("-s", "--save", action="store_true", help="save all info to file")
-# ap.add_argument("-i", "--info", action="store_true", help="reverse lookup img / media")
 args = vars(ap.parse_args())
 	
 os.system("clear")
@@ -20,10 +17,3 @@ if args['user']:
 
 if args['post']:
 	post_info()
-
-# if args['save']:
-# 	pass
-# if args['tor']:
-# 	pass
-# if args['info']:
-# 	pass

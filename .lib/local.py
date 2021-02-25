09642c6773e991aef3f63113580717df1597f7c8
th3unkn0n <40244622+th3unkn0n@users.noreply.github.com>
2021-02-25 20:44:17 +0530

    Update local.py

--- .lib/local.py
+++ .lib/local.py
@@ -59,7 +59,7 @@ def banner():
  ╚═╝  ╚═╝  ╩  {gr}o{cy}  ╩  ╚═╝
  
         {gr}Code By :
-  {gr}youtube.com/theunknon{nu}
+  {gr}youtube.com/UCnknCgg_3pVXS27ThLpw3xQ{nu}
 	            """)
 
 useragent = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
